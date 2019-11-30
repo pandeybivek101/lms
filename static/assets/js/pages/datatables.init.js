0913a1bc78353995c45b59457e74c1e7f0342711
Bivek Pandey <pandeyvivak25@gmail.com>
2019-11-30 14:49:31 +0545

    changes in datatables

--- static/assets/js/pages/datatables.init.js
+++ static/assets/js/pages/datatables.init.js
@@ -1,1 +1,36 @@
 $(document).ready(function(){$("#datatable").DataTable();var a=$("#datatable-buttons").DataTable({lengthChange:!1,buttons:["copy","excel","pdf"]});$("#key-table").DataTable({keys:!0}),$("#responsive-datatable").DataTable(),$("#selection-datatable").DataTable({select:{style:"multi"}}),a.buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)")});
+
+
+$(document).ready(function() {
+  $('#selection-datatable1').DataTable( {
+    dom: "Blfrtip",
+    lengthMenu: [
+    [ 5, 10, 20, -1 ],
+    [ '5 rows', '10 rows', '20 rows', 'All' ]
+    ],
+    buttons: [
+    'copy',
+    'csv',
+    'excel',
+    'pdf',
+    'pageLength',
+    {
+        extend: 'print',
+        text: 'Print all',
+        exportOptions: {
+            modifier: {
+                selected: null
+            }
+        }
+    },
+    {
+        extend: 'print',
+        text: 'Print selected'
+    }
+    ],
+    select:{
+        style:"multi",
+    },
+
+} );
+} );
